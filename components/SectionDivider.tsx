777fcdb59a3e74210ab502640e139f035eb98b34
JustMakingUpWords <duc.vo.hm@gmail.com>
2024-02-02 17:36:11 -0600

    section divider animation
    moving up and down slowly

--- components/SectionDivider.tsx
+++ components/SectionDivider.tsx
@@ -2,17 +2,35 @@
 
 import React from "react";
 import { motion } from "framer-motion";
-
+/*
+initial={{ y: 100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{
+        duration: 1
+      }}
+*/
 export default function SectionDivider() {
   return (
     <motion.div
       initial={{ y: 100, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{
+        type: "tween",
         duration: 1
-      }}
-      className="bg-neutral-700 my-28 h-20 w-1
-    rounded-full
-    hidden sm:block"></motion.div>
+      }}>
+      <motion.div
+        animate={{
+          y: [-15, 15, -15]
+        }}
+        transition={{
+          ease: "easeInOut",
+          duration: 5,
+          repeat: Infinity,
+          delay: 1
+        }}
+        className="bg-neutral-700 my-32 h-20 w-1
+      rounded-full
+      hidden sm:block"></motion.div>
+    </motion.div>
   );
 }
