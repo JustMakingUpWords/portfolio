2e3c3b2dca47bef20a16470e087800bd0bfb2467
JustMakingUpWords <duc.vo.hm@gmail.com>
2024-02-03 02:48:26 -0600

    updated mail icon and other icons

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -76,8 +76,8 @@ export default function Intro() {
           <a href="https://github.com/JustMakingUpWords" target="_blank">
             <FaGithubSquare
               className="size-9 
-          hover:outline hover:mx-1 outline-3 outline-offset-[0.1rem] rounded-[0.2rem]
-          duration-100 "></FaGithubSquare>
+            hover:outline outline-3 outline-offset-[0.1rem] rounded-[0.2rem]
+            duration-100"></FaGithubSquare>
           </a>
         </motion.div>
 
@@ -92,8 +92,8 @@ export default function Intro() {
           <a href="https://www.linkedin.com/" target="_blank">
             <FaLinkedin
               className="size-9 
-          hover:outline hover:mx-1 outline-3 outline-offset-[0.1rem] rounded-[0.2rem]
-          duration-100 "></FaLinkedin>
+            hover:outline outline-3 outline-offset-[0.1rem] rounded-[0.2rem]
+            duration-100"></FaLinkedin>
           </a>
         </motion.div>
 
@@ -107,9 +107,9 @@ export default function Intro() {
           }}>
           <a href="https://twitter.com/home" target="_blank">
             <FaTwitterSquare
-              className="size-9 
-          hover:outline hover:mx-1 outline-3 outline-offset-[0.1rem] rounded-[0.2rem]
-          duration-100 "></FaTwitterSquare>
+              className="size-9 mr-[0.05rem]
+            hover:outline outline-3 outline-offset-[0.1rem] rounded-[0.2rem]
+            duration-100"></FaTwitterSquare>
           </a>
         </motion.div>
 
@@ -120,15 +120,36 @@ export default function Intro() {
             type: "spring",
             delay: 0.26,
             duration: 0.7
-          }}>
-          <a href="https://twitter.com/home" target="_blank">
-            <ImMail
-              className="size-8
-          hover:outline hover:mx-1 outline-3 outline-offset-[0.1rem] rounded-[0.2rem]
-          duration-100 "></ImMail>
-          </a>
+          }}
+          className="group">
+          <ImMail // stand in for mail icon
+            className="size-[1.92rem] rounded-[0.2rem]"></ImMail>
+          <div // actual mail thing (i did this so the bar scales to the right and doesnt affect anything else)
+            className="absolute
+          -translate-y-[1.95rem] translate-x-[0.01rem]
+          group block size-[1.96rem]
+        bg-slate-100 rounded-[0.2rem] bg-opacity-0
+        text-black text-opacity-0 font-sans tracking-tight font-semibold
+          group-hover:outline outline-white outline-offset-[0.2rem]
+          group-hover:w-[10.5rem] group-hover:text-opacity-100 group-hover:bg-opacity-100
+          duration-200">
+            <div // words
+              className="absolute
+            translate-y-[0.19rem] translate-x-[0.3rem]
+            hidden group-hover:block">
+              duc.vo.hm@gmail.com
+            </div>
+          </div>
         </motion.div>
       </div>
     </section>
   );
 }
+/*
+<a href="https://twitter.com/home" target="_blank">
+    <ImMail
+      className="size-8 z-50
+hover:outline hover:mx-1 outline-3 outline-offset-[0.1rem] rounded-[0.2rem]
+duration-100"></ImMail>
+</a>
+*/
