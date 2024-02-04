f1e92526886bde0e3f2f86e72f216e4f32eb9450
JustMakingUpWords <duc.vo.hm@gmail.com>
2024-02-03 20:19:24 -0600

    created About section

---
+++ components/About.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import SectionHeading from "@/components/SectionHeading";
+import React from "react";
+import { animate, motion } from "framer-motion";
+
+export default function About() {
+  return (
+    <motion.section
+      initial={{ y: 50, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.7 }}
+      viewport={{ once: true }}
+      className="my-10 max-w-[30rem]
+    text-center leading-8
+    sm:mb-40 sm:max-w-[45rem]">
+      <SectionHeading>About Me</SectionHeading>
+      <p className="mb-3">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </motion.section>
+  );
+}
