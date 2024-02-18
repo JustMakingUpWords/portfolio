ff3e5ac877978fde79d7e5209be15835e0cb5ddd
JustMakingUpWords <duc.vo.hm@gmail.com>
2024-02-18 01:34:27 -0600

    created utils.ts

---
+++ lib/utils.ts
@@ -0,0 +1,19 @@
+export const validateForm = (value: unknown, maxLength: number) => {
+  return value && typeof value === "string" && value.length <= maxLength;
+};
+
+export const getErrorMessage = (error: unknown): string => {
+  let message: string;
+
+  if (error instanceof Error) {
+    message = error.message;
+  } else if (error && typeof error === "object" && "message" in error) {
+    message = String(error.message);
+  } else if (typeof error === "string") {
+    message = error;
+  } else {
+    message = "Unknown error";
+  }
+
+  return message;
+};
