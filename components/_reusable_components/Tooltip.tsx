870032ad71758a514aacc51b4e0bad8b6fde9979
JustMakingUpWords <duc.vo.hm@gmail.com>
2024-02-18 20:25:23 -0600

    fixed tooltips

--- components/_reusable_components/Tooltip.tsx
+++ components/_reusable_components/Tooltip.tsx
@@ -3,10 +3,11 @@ import { motion } from "framer-motion";
 
 type ToolTipProps = {
   text: String;
+  transX: String;
   children: React.ReactNode;
 };
 
-export default function Tooltip({ text, children }: ToolTipProps) {
+export default function Tooltip({ text, transX, children }: ToolTipProps) {
   const [isVisible, setIsVisible] = useState(false);
 
   return (
@@ -21,10 +22,20 @@ export default function Tooltip({ text, children }: ToolTipProps) {
       {children}
       {isVisible && (
         <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.3 }}
-          className="absolute bg-white -translate-x-[40%] text-[1rem] mt-2">
+          initial={{
+            opacity: 0,
+            y: -10,
+            x: `${transX}`,
+            scale: 0.9
+          }}
+          animate={{
+            opacity: 1,
+            y: 0,
+            x: `${transX}`,
+            scale: 1
+          }}
+          transition={{ duration: 0.2 }}
+          className="absolute bg-white text-[1rem] mt-[0.35rem] text-black rounded-lg px-2">
           {text}
         </motion.div>
       )}
