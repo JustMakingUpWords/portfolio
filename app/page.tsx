b8533acea48628efaf89006b2ac0ec2eb13b9dd4
JustMakingUpWords <duc.vo.hm@gmail.com>
2024-02-24 01:18:54 -0600

    created background component

--- app/page.tsx
+++ app/page.tsx
@@ -7,28 +7,12 @@ import Intro from "@/components/Intro_C/Intro";
 import Projects from "@/components/Project_C/Projects";
 import Skills from "@/components/Skills_C/Skills";
 import SectionDivider from "@/components/_reusable_components/SectionDivider";
-import bg_dark from "@/public/bg_dark.jpeg";
-import bg_dark2 from "@/public/bg_dark2.jpg";
-import bg_dark3 from "@/public/bg_dark3.jpg";
-
-/*
-<Image
-          src={bg_dark3}
-          alt="background"
-          width={1000}
-          objectFit="cover"
-          className="brightness-75"></Image>
-*/
+import Background from "@/components/Background_C/Background";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
-      <div className="hidden dark:block absolute w-[100%] h-[100%] -z-10 overflow-hidden brightness-50 blur-3xl">
-        <img src="bg_dark.jpg" alt="background"></img>
-      </div>
-      <div className="block dark:hidden absolute w-[100%] h-[100%] -z-10 overflow-hidden brightness-100 blur-3xl">
-        <img src="bg_light.png" alt="background"></img>
-      </div>
+      <Background></Background>
       <div className="mb-[12rem]"></div>
       <Intro></Intro>
       <SectionDivider>{2}</SectionDivider>
---
+++ components/Background_C/Background.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+
+export default function Background() {
+  return (
+    <section>
+      <div
+        className="hidden dark:block absolute
+      w-[100%] h-[100%] -translate-x-[50%] -z-10
+      overflow-hidden brightness-50 blur-3xl">
+        <img src="bg_dark.jpg" alt="background"></img>
+      </div>
+      <div
+        className="block dark:hidden absolute
+      w-[100%] h-[100%] -translate-x-[50%] -z-10
+      overflow-hidden brightness-100 blur-3xl">
+        <img src="bg_light.png" alt="background"></img>
+      </div>
+    </section>
+  );
+}
